2a841ee06a588b6993acb06ae0f5eb40f9329012
TeodoraIlic97 <teodorailicns97@gmail.com>
2019-11-06 20:03:53 +0100

    Dodavanje nekih modela

---
+++ KlinickiCentar/src/main/java/com/klinickiCentar/Modeli/AdministratorKlinickogCentra.java
@@ -0,0 +1,12 @@
+package com.klinickiCentar.Modeli;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class AdministratorKlinickogCentra {
+
+	//List<Klinika> klinike;
+	 List<Korisnik> korisnika;  //ne odobrenih
+	 HashMap<Integer, String> dijagnoza;
+	//Lek lek;
+}
