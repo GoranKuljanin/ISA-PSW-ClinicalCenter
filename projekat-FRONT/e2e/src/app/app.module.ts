11cdb942c80f654f98dc36a66ffb83ed6e9f06bd
VesnaK193 <vesna.karaklic.97@gmail.com>
2019-11-28 13:51:03 +0100

    Profil lekara
    Uradjen prikaz profila lekara, tabelarni prikaz i pretrazivanje pacijenata.

---
+++ projekat-FRONT/e2e/src/app/app.module.ts
@@ -0,0 +1,71 @@
+import { PacijentService } from './services/pacijentServices/pacijent.service';
+import { AdminKlinickogCentraService } from './services/adminKCServices/admin-klinickog-centra.service';
+import { LoginService } from './services/login.service';
+import { RegisterServiceService } from './services/register-service.service';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AngularMaterialModule } from './angular-material.module';
+
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { KorisnikService } from './services/korisnik.service';
+import { AdminKlinickogCentraHomePageComponent } from './components/admin-klinickog-centra-home-page/admin-klinickog-centra-home-page.component';
+import { ListaRegistrovanihZahtevaComponent } from './components/admin-klinickog-centra-home-page/adminComponents/lista-registrovanih-zahteva/lista-registrovanih-zahteva.component';
+import { ListaPacijenataComponent } from './components/admin-klinickog-centra-home-page/adminComponents/lista-pacijenata/lista-pacijenata.component';
+import { ListaKlinikaComponent } from './components/lista-klinika/lista-klinika.component';
+import { PrikazKlinikeComponent } from './components/lista-klinika/prikaz-klinike/prikaz-klinike.component';
+import { PacijentHomePageComponent } from './components/pacijent-home-page/pacijent-home-page.component';
+import { PodaciPacijentaComponent } from './components/pacijent-home-page/podaci-pacijenta/podaci-pacijenta.component';
+import { ZakazaniPreglediComponent } from './components/pacijent-home-page/zakazani-pregledi/zakazani-pregledi.component';
+import { EditProfilDialogComponent } from './components/pacijent-home-page/podaci-pacijenta/edit-profil-dialog/edit-profil-dialog.component';
+import { LekarComponent } from './components/lekar/lekar.component';
+import { ZapocniPregledComponent } from './components/lekar/components/zapocni-pregled/zapocni-pregled.component';
+import { RadniKalendarComponent } from './components/lekar/components/radni-kalendar/radni-kalendar.component';
+import { ZakaziPregledComponent } from './components/lekar/components/zakazi-pregled/zakazi-pregled.component';
+import { ZahtevZaGodisnjiComponent } from './components/lekar/components/zahtev-za-godisnji/zahtev-za-godisnji.component';
+import { ProfilComponent } from './components/lekar/components/profil/profil.component';
+import { PacijentiComponent } from './components/lekar/components/pacijenti/pacijenti.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    AdminKlinickogCentraHomePageComponent,
+    ListaRegistrovanihZahtevaComponent,
+    ListaPacijenataComponent,
+    ListaKlinikaComponent,
+    PrikazKlinikeComponent,
+    PacijentHomePageComponent,
+    PodaciPacijentaComponent,
+    ZakazaniPreglediComponent,
+    EditProfilDialogComponent,
+    LekarComponent,
+    ZapocniPregledComponent,
+    RadniKalendarComponent,
+    ZakaziPregledComponent,
+    ZahtevZaGodisnjiComponent,
+    ProfilComponent,
+    PacijentiComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule,
+    FlexLayoutModule, 
+    FormsModule, 
+    ReactiveFormsModule 
+  ],
+  entryComponents: [EditProfilDialogComponent],
+  providers: [KorisnikService, RegisterServiceService, LoginService, AdminKlinickogCentraService, PacijentService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
