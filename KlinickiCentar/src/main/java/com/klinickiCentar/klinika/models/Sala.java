e90597966dc4b9fe787d17edbbae90fa3f973a3b
VesnaK193 <vesna.karaklic.97@gmail.com>
2019-12-03 19:50:22 +0100

    Profil lekara
    Implementiran kod za profil lekara i za prikaz i pretrazivanje pacijenata

---
+++ KlinickiCentar/src/main/java/com/klinickiCentar/klinika/models/Sala.java
@@ -0,0 +1,58 @@
+package com.klinickiCentar.klinika.models;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "sala")
+public class Sala {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "name", nullable = false)
+	private String name;
+	
+	@ManyToOne
+	@JoinColumn(name="klinika")
+	private Klinika klinika;
+
+	public Sala() {
+		super();
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Klinika getKlinika() {
+		return klinika;
+	}
+
+	public void setKlinika(Klinika klinika) {
+		this.klinika = klinika;
+	}
+	
+	
+}
---
+++ projekat-FRONT/src/app/models/pacijent.ts
@@ -0,0 +1,5 @@
+import { User } from './user.model';
+export class Pacijent {
+    zdravstveniKarton:string;
+    user:User;
+}
