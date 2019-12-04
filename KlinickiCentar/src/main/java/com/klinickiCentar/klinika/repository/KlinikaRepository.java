4b24dc94a3a01038c507e2c32d6bd0a568e1fea9
GoranKuljanin <goku.kuljanin@gmail.com>
2019-12-04 23:10:12 +0100

    Obrisan Klinicki Centar

--- KlinickiCentar/src/main/java/com/klinickiCentar/klinika/repository/KlinikaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.klinickiCentar.klinika.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.klinickiCentar.klinika.models.Klinika;
-
-public interface KlinikaRepository extends JpaRepository<Klinika, Long>{
-
-}
