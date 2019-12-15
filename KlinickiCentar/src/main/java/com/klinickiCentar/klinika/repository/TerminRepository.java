a0168c74b52d0a2d85e3c5d123e37f1216962204
GoranKuljanin <goku.kuljanin@gmail.com>
2019-12-15 14:31:57 +0100

    Obrisan Klinicki Centar

--- KlinickiCentar/src/main/java/com/klinickiCentar/klinika/repository/TerminRepository.java
+++
@@ -1,8 +0,0 @@
-package com.klinickiCentar.klinika.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.klinickiCentar.klinika.models.Termin;
-
-public interface TerminRepository extends JpaRepository<Termin, Long> {
-}
