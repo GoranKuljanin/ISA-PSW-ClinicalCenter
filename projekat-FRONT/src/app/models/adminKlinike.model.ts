9b57b67b45aba172f80488044fe1e27ec2ee1212
GoranKuljanin <goku.kuljanin@gmail.com>
2020-02-05 13:32:46 +0100

    Obrisan Angular

--- projekat-FRONT/src/app/models/adminKlinike.model.ts
+++
@@ -1,9 +0,0 @@
-import { User } from './user.model';
-import { Klinika } from './klinika.model';
-
-export class AdminKlinike{
-    id:number;
-    user:User;
-    klinika:Klinika;
-    firstLogin:boolean;
-}
