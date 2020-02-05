07fb64c555b131b306c74489f0dc8b503bb74593
GoranKuljanin <goku.kuljanin@gmail.com>
2020-02-05 13:32:22 +0100

    Obrisan Klinicki Centar

--- KlinickiCentar/src/main/java/com/klinickiCentar/klinika/repository/SalaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.klinickiCentar.klinika.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.klinickiCentar.klinika.models.Sala;
-
-public interface SalaRepository extends JpaRepository<Sala, Long> {
-}
